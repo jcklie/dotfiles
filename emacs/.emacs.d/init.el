b987a639f69e1c55a997bb73fcbf4f712cc3d093
Jan-Christoph Klie <git@mrklie.com>
2018-11-30 09:31:40 +0100

    Add emacs

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,55 @@
+(require 'package)
+
+(defvar melpa '("melpa" . "https://melpa.org/packages/"))
+(defvar melpa-stable '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(defvar org-elpa '("org" . "https://orgmode.org/elpa/"))
+
+(add-to-list 'package-archives melpa-stable t)
+(add-to-list 'package-archives melpa t)
+(add-to-list 'package-archives org-elpa t)
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("aaffceb9b0f539b6ad6becb8e96a04f2140c8faa1de8039a343a4f1e009174fb" default)))
+ '(org-agenda-files
+   (quote
+    ("/home/klie/notes/topics/someday.org" "/home/klie/notes/topics/listen-read-watch.org" "/home/klie/notes/projects/chinese.org" "/home/klie/notes/dates/2018-november.org" "/home/klie/notes/journal/2018_48.org")))
+ '(package-selected-packages (quote (evil gruvbox-theme dracula-theme use-package org))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; 'use-package' settings (from use-package README)
+(eval-when-compile
+  (require 'use-package))
+
+(use-package org
+  :init
+  (load-library "find-lisp")
+  (setq org-agenda-files (find-lisp-find-files "~/notes" "\\.org$"))
+  :bind
+  (("C-c a" . org-agenda))
+)
+
+;; System locale to use for formatting time values.
+(setq system-time-locale "C")
+
+;; Set theme
+(load-theme 'gruvbox t)
+
+;; Disable menu bar
+(menu-bar-mode -1)
