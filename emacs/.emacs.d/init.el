8023edc14ff351137b06f86a7f5ce85effe9ad93
Jan-Christoph Klie <git@mrklie.com>
2019-01-04 11:40:35 +0100

    use org init file

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -15,7 +15,7 @@
     ("/home/klie/notes/topics/someday.org" "/home/klie/notes/topics/listen-read-watch.org" "/home/klie/notes/projects/chinese.org" "/home/klie/notes/dates/2018-november.org" "/home/klie/notes/journal/2018_48.org")) t)
  '(package-selected-packages
    (quote
-    (org-bullets gruvbox evil gruvbox-theme dracula-theme use-package org))))
+    (avy org-bullets gruvbox evil gruvbox-theme dracula-theme use-package org))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -47,54 +47,4 @@
 (eval-when-compile
   (require 'use-package))
 
-;; Set theme
-(use-package gruvbox-theme
-  :ensure t
-  :init
-  (load-theme 'gruvbox t))
-
-(use-package org
-  :init
-  (load-library "find-lisp")
-  (setq org-agenda-files (find-lisp-find-files "~/notes" "\\.org$"))
-  :bind
-  (("C-c a" . org-agenda))
-  )
-
-(use-package org-bullets
-  :ensure t
-  :config
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
-  
-
-;; Use utf-8
-(set-language-environment "UTF-8")
-
-;; Disable menu bar
-(menu-bar-mode -1)
-
-;; System locale to use for formatting time values.
-(setq system-time-locale "C")
-
-;; Enable ido mode
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(setq ido-use-virtual-buffers t)
-(ido-mode 1)
-
-;; use ibuffer instead of list-buffers
-(defalias 'list-buffers 'ibuffer)
-
-;; Use y and n for yes and no
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-;; Disable bell
-(setq ring-bell-function 'ignore)
-
-;; Disable backups and autosaves
-(setq make-backup-file nil)
-(setq auto-save-default nil)
-
-;; Show column and line number
-(line-number-mode 1)
-(column-number-mode 1)
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
